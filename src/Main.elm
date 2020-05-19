05a99ab42547bc43607d9a2606aa541f3dfc18f3
Jose Nelson Alcocer Amurrio <nelsontloz@gmail.com>
2020-05-20 00:17:40 +0100

    initial commit.

--- src/Main.elm
+++ src/Main.elm
@@ -3,31 +3,73 @@ module Main exposing (main)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
 
 main =
-    div [class "container"] [
-        a [class "waves-effect waves-light btn"] [text "a button"],
-        h1 [] [ text "Hello, Elm!" ],
-        a [class "waves-effect waves-light btn modal-trigger" , href "#modal"] [text "Modal"]
-        
-    ]
+    Browser.sandbox { init = init, update = update, view = view }
 
 
+type alias Model =
+    { name : String
+    , password : String
+    , confirmPassword : String
+    }
 
-    -- <!-- Modal Trigger -->
-    -- <a class="waves-effect waves-light btn modal-trigger" href="#modal1"
-    --   >Modal</a
-    -- >
 
-    -- <!-- Modal Structure -->
-    -- <div id="modal1" class="modal">
-    --   <div class="modal-content">
-    --     <h4>Modal Header</h4>
-    --     <p>A bunch of text</p>
-    --   </div>
-    --   <div class="modal-footer">
-    --     <a href="#!" class="modal-close waves-effect waves-green btn-flat"
-    --       >Agree</a
-    --     >
-    --   </div>
-    -- </div>
+init : Model
+init =
+    Model "" "" ""
+
+
+type Msg
+    = ChangeName String
+    | ChangePassword String
+    | ChangeConfirmPassword String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        ChangeName newName ->
+            { model | name = newName }
+
+        ChangePassword newPassword ->
+            { model | password = newPassword }
+
+        ChangeConfirmPassword newPassword ->
+            { model | confirmPassword = newPassword }
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "container" ]
+        [ div [ class "field" ]
+            [ label [ class "label" ] [ text "Name" ]
+            , div [ class "control" ]
+                [ viewInput model "text" "Name" model.name ChangeName
+                ]
+            ]
+        , div [ class "field" ]
+            [ label [ class "label" ] [ text "Password" ]
+            , div [ class "control" ]
+                [ viewInput model "password" "Password" model.password ChangePassword
+                ]
+            ]
+        , div [ class "field" ]
+            [ label [ class "label", classList [ ( "is-success", passwordsMatch model.password model.confirmPassword ) ] ] [ text "Confirm password" ]
+            , div [ class "control" ]
+                [ viewInput model "password" "Confirm Password" model.confirmPassword ChangeConfirmPassword
+                ]
+            ]
+        ]
+
+
+viewInput : Model -> String -> String -> String -> (String -> msg) -> Html msg
+viewInput model t p v toMsg =
+    input [ class "input", type_ t, placeholder p, value v, onInput toMsg, classList [ ( "is-success", passwordsMatch model.password model.confirmPassword ) ] ] []
+
+
+passwordsMatch : String -> String -> Bool
+passwordsMatch password confirmPassword =
+    password == confirmPassword
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import { Elm } from './Main.elm';
 import './index.scss';
-import * as M from '../node_modules/materialize-css/dist/js/materialize';
-
-M.AutoInit();
 
 Elm.Main.init({
   node: document.querySelector('main'),
